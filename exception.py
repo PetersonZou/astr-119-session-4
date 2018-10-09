e37f6d1fd5b87a260512796c3cdae9a07b30994b
PetersonZou <43654434+PetersonZou@users.noreply.github.com>
2018-10-09 13:12:04 -0700

    Uploaded codes from session 4

---
+++ exception.py
@@ -0,0 +1,17 @@
+#python exceptions deal with unexpected results
+
+try:
+	print(a)		#this will throw an exception since a is not defined
+except:
+	print("a is not defined!")
+	
+#there are specific errors to help with cases
+try:
+	print(a)		#this will throw an exception since a is not defined
+except NameError:
+	print("a is still not defined!")
+except:
+	print("Something else went weong.")
+	
+#this will break our program since a is not defined
+print(a)
